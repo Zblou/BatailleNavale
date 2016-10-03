2367f0917253de2bfc85c335a3d0b1f78e6b9610
Tanguy Leborgne <Tanguy@macbook-pro-de-tanguy.local>
2016-10-03 12:18:19 +0200

    Initial Commit

---
+++ Cours1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Cours1
+//
+//  Created by Tanguy Leborgne on 03/10/2016.
+//  Copyright © 2016 Tog. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
